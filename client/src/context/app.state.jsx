3b2e71a4a15810d79109d3d5c81d64566bdc22e5
jwebster7 <>
2021-01-30 16:32:32 -0600

    did more stuff

--- client/src/context/app.state.jsx
+++ client/src/context/app.state.jsx
@@ -1,15 +1,4 @@
 export const initialState = {
-  weather: {
-    current: {
-      feels_like: 0,
-      humidity: 0,
-      pressure: 0,
-      temp: 0,
-      temp_max: 0,
-      temp_min: 0,
-      speed: 0
-    }
-  },
   current: {
     feels_like: 0,
     humidity: 0,
