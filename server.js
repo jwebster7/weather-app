3959457c446a6c93f79a195028199c03bb1f0ed2
jwebster7 <>
2021-02-02 15:36:55 -0600

    that fix did not work

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
 
   // app.get() defines what happens when the server receives HTTP GET requests
   // '*' = from any URL
-  const home = ["/", "/home", "/*"];
+  const home = ["/", "/home"];
   app.get(home, function (req, res) {
     // sends the index.html (which is static) that includes the whole client app code.
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
